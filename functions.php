8c187ee8b6713f94380042103b0611d38141e1a6
Thesabel Tuto <thesabeltuto@gmail.com>
2016-07-02 04:32:50 +0800

    1.1.2

--- functions.php
+++ functions.php
@@ -6,12 +6,12 @@ define('THEME_CHILD_DIR', get_template_directory().$THEME_CHILD);
 define('THEME_CHILD_URL', get_template_directory_uri().$THEME_CHILD);
 
 // Global variables
-$THEME_VERSION = '1.1.1';
+$THEME_VERSION = '1.1.2';
 $THEME_CSS_VERSION = '1.0.8';
 
-require_once(THEME_DIR.'/framework/admin.php');
-require_once(THEME_DIR.'/framework/integrations.php');
-require_once(THEME_DIR.'/framework/shortcodes.php');
+require_once(THEME_CHILD_DIR.'/framework/admin.php');
+require_once(THEME_CHILD_DIR.'/framework/integrations.php');
+require_once(THEME_CHILD_DIR.'/framework/shortcodes.php');
 
 //error_reporting(E_ALL); ini_set('display_errors', 1);
 ?>
