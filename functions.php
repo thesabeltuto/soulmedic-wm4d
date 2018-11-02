9d0333bd672f58a076ba7e245f617f769b762672
Thesabel Tuto <thesabeltuto@gmail.com>
2018-11-02 22:00:58 +0800

    1.1.7

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ define('THEME_CHILD_DIR', get_template_directory().$THEME_CHILD);
 define('THEME_CHILD_URL', get_template_directory_uri().$THEME_CHILD);
 
 // Global variables
-$THEME_VERSION = '1.1.6';
-$THEME_CSS_VERSION = '1.1.0';
+$THEME_VERSION = '1.1.7';
+$THEME_CSS_VERSION = '1.1.1';
 
 require_once(THEME_CHILD_DIR.'/framework/admin.php');
 
